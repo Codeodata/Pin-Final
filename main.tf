799bda6e9cef24b4894b0706c63b2875b1486eb0
Diego <diego.lavezzari@gmail.com>
2025-03-07 17:53:33 -0300

    creacion de bucket desde main.tf

--- main.tf
+++ main.tf
@@ -4,9 +4,40 @@ resource "aws_instance" "monitoring" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  user_data                   = file("Contenedores.sh")  # Ejecutar el script al lanzar la instancia
+  user_data                   = file("metrics.sh")  # Ejecutar el script al lanzar la instancia
 
   tags = {
     Name = "monitoring"
+  }
+}
+
+# Crear el bucket S3
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = var.bucket_name
+  force_destroy = true
+
+  tags = {
+    Name        = "Terraform State Bucket"
+    Environment = "Dev"
+  }
+}
+
+# Habilitar versionado en el bucket
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Configurar cifrado del lado del servidor (SSE)
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
