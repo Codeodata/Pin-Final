8b0a2febbdaa0ab56d1e5a9cb26a2a724418f240
Diego <diego.lavezzari@gmail.com>
2025-03-09 15:40:52 -0300

    Se corrigen varios archivos de terraform y terraform.yml

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
 provider "aws" {
   region = var.region  # Usando la variable de región
 }
-
-# Configuración del Backend para almacenar el estado en S3
-terraform {
-  backend "s3" {
-    bucket = "terraform-state"  # Nombre fijo del bucket
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    acl     = "bucket-owner-full-control"
-  }
-}
