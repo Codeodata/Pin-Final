d30be39c73523b2250faade454843d1f114e02d4
Diego <diego.lavezzari@gmail.com>
2025-03-09 14:34:09 -0300

    Se borra creacion del bucket en el main

--- main.tf
+++ main.tf
@@ -7,26 +7,6 @@ resource "aws_instance" "monitoring" {
   user_data                   = file("metrics.sh")  # Ejecutar el script al lanzar la instancia
 
   tags = {
-    Name = "monitoring"
-  }
-}
-
-# Habilitar versionado en el bucket
-resource "aws_s3_bucket_versioning" "versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# Configurar cifrado del lado del servidor (SSE)
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
+    Name = var.instance_name
   }
 }
